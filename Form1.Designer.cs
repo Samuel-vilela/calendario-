5d18b8226292d5674f602ce6a0e1127810d1d711
samuel.varaujo <samuel.varaujo@AME0613343W10-1>
2024-05-02 22:20:11 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace calendario_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDatainicial = new System.Windows.Forms.TextBox();
+            this.textBoxDataFinal = new System.Windows.Forms.TextBox();
+            this.textBoxDataAtual = new System.Windows.Forms.TextBox();
+            this.btnPegarData = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxAutoinicial = new System.Windows.Forms.TextBox();
+            this.textBoxAutoFinal = new System.Windows.Forms.TextBox();
+            this.textBoxAutoHoje = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(37, 71);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowToday = false;
+            this.monthCalendar1.TabIndex = 1;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(422, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Data inicial:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(426, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data final:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(426, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Data atual:";
+            // 
+            // textBoxDatainicial
+            // 
+            this.textBoxDatainicial.Location = new System.Drawing.Point(520, 71);
+            this.textBoxDatainicial.Name = "textBoxDatainicial";
+            this.textBoxDatainicial.Size = new System.Drawing.Size(163, 26);
+            this.textBoxDatainicial.TabIndex = 5;
+            // 
+            // textBoxDataFinal
+            // 
+            this.textBoxDataFinal.Location = new System.Drawing.Point(520, 127);
+            this.textBoxDataFinal.Name = "textBoxDataFinal";
+            this.textBoxDataFinal.Size = new System.Drawing.Size(163, 26);
+            this.textBoxDataFinal.TabIndex = 6;
+            // 
+            // textBoxDataAtual
+            // 
+            this.textBoxDataAtual.Location = new System.Drawing.Point(520, 180);
+            this.textBoxDataAtual.Name = "textBoxDataAtual";
+            this.textBoxDataAtual.Size = new System.Drawing.Size(163, 26);
+            this.textBoxDataAtual.TabIndex = 7;
+            // 
+            // btnPegarData
+            // 
+            this.btnPegarData.Location = new System.Drawing.Point(426, 261);
+            this.btnPegarData.Name = "btnPegarData";
+            this.btnPegarData.Size = new System.Drawing.Size(257, 35);
+            this.btnPegarData.TabIndex = 8;
+            this.btnPegarData.Text = "Selecione Data ";
+            this.btnPegarData.UseVisualStyleBackColor = true;
+            this.btnPegarData.Click += new System.EventHandler(this.btnPegarData_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 353);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Data inicial:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 395);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Data Final:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 443);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Data Atual:";
+            // 
+            // textBoxAutoinicial
+            // 
+            this.textBoxAutoinicial.Location = new System.Drawing.Point(129, 353);
+            this.textBoxAutoinicial.Name = "textBoxAutoinicial";
+            this.textBoxAutoinicial.Size = new System.Drawing.Size(158, 26);
+            this.textBoxAutoinicial.TabIndex = 12;
+            // 
+            // textBoxAutoFinal
+            // 
+            this.textBoxAutoFinal.Location = new System.Drawing.Point(130, 395);
+            this.textBoxAutoFinal.Name = "textBoxAutoFinal";
+            this.textBoxAutoFinal.Size = new System.Drawing.Size(157, 26);
+            this.textBoxAutoFinal.TabIndex = 13;
+            // 
+            // textBoxAutoHoje
+            // 
+            this.textBoxAutoHoje.Location = new System.Drawing.Point(129, 443);
+            this.textBoxAutoHoje.Name = "textBoxAutoHoje";
+            this.textBoxAutoHoje.Size = new System.Drawing.Size(157, 26);
+            this.textBoxAutoHoje.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 642);
+            this.Controls.Add(this.textBoxAutoHoje);
+            this.Controls.Add(this.textBoxAutoFinal);
+            this.Controls.Add(this.textBoxAutoinicial);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPegarData);
+            this.Controls.Add(this.textBoxDataAtual);
+            this.Controls.Add(this.textBoxDataFinal);
+            this.Controls.Add(this.textBoxDatainicial);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDatainicial;
+        private System.Windows.Forms.TextBox textBoxDataFinal;
+        private System.Windows.Forms.TextBox textBoxDataAtual;
+        private System.Windows.Forms.Button btnPegarData;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxAutoinicial;
+        private System.Windows.Forms.TextBox textBoxAutoFinal;
+        private System.Windows.Forms.TextBox textBoxAutoHoje;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calendario_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPegarData_Click(object sender, EventArgs e)
+        {
+            textBoxDatainicial.Text = monthCalendar1.SelectionStart.ToShortDateString();
+            textBoxDataFinal.Text = monthCalendar1.SelectionEnd.ToShortDateString();
+            textBoxDataAtual.Text = monthCalendar1.TodayDate.ToShortDateString();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            textBoxAutoinicial.Text = monthCalendar1.SelectionStart.ToShortDateString();
+            textBoxAutoFinal.Text = monthCalendar1.SelectionEnd.ToShortDateString();
+            textBoxAutoHoje.Text = monthCalendar1.TodayDate.ToShortDateString();
+        }
+    }
+}
